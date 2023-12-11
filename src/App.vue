935e426460d6cd67452ee98245270fab37440111
StefaneSantos45 <seu.email@example.com>
2023-12-11 15:22:52 -0300

    CursorFollower

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+   <CursorFollower/>
+  </div>
+</template>
+
+<script>
+import CursorFollower from "@/components/CursorFollower.vue"
+export default {
+  data() {
+    return {
+      
+    };
+  },
+  components:{
+    CursorFollower
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
